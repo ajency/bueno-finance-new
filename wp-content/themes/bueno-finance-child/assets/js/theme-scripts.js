2ff0da42b41280e16e5ae24df228d98e048ce811
Siddesh <siddesh@ajency.in>
2022-02-24 15:36:07 +0530

    scan and pay section

--- wp-content/themes/bueno-finance-child/assets/js/theme-scripts.js
+++ wp-content/themes/bueno-finance-child/assets/js/theme-scripts.js
@@ -4,3 +4,18 @@ $(window).on("load resize",function(e){
     $containerMargin = (($(".site-container").innerWidth()) - ($(".why-bueno-section .alignwide").innerWidth()))/2;
     $(".why-bueno-section .gb-container-image-wrap .gb-container-image").css("width", $columnWidth + $containerMargin);
 });
+$(document).ready(function(){
+    $('.mobile-slider-js').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        speed: 1000,
+        fade: true,
+        cssEase: 'ease-in-out',
+        touchThreshold: 100
+    });
+});
